a96a56e569c1955759c49f46b01fb45b8ebeaa8b
bongjun-kim94 <74786253+bongjun-kim94@users.noreply.github.com>
2021-04-29 02:30:25 +0900

    portfolio

---
+++ js/slider.js
@@ -0,0 +1,17 @@
+$(function(){
+
+        $(".regular").slick({
+            dots: false,
+            infinite: true,
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            arrows:true
+        // fade:true
+        // autoplay:true,
+        // autoplaySpeend:1500,
+        // speend:1000
+        // centerMode: true,
+        // vertical: true
+        });
+
+});
